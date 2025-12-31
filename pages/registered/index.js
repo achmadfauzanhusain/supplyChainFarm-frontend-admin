919af7e5198c3dc64661c5c7e4be0ed8532c490b
Fauzan Husain <achmadfauzan1340@gmail.com>
2026-01-01 01:59:24 +0800

    api(integrated): integrated API registeredSupplier

--- pages/registered/index.js
+++ pages/registered/index.js
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-import Navbar from "@/components/navbar";
 import { Urbanist } from "next/font/google"
-
 const urbanist = Urbanist({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800"] });
 
+import { registeredSuppliers } from "@/services/supplier";
+
 const Registered = () => {
+    const [suppliers, setSuppliers] = useState([])
+
     const router = useRouter()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await registeredSuppliers()
+            setSuppliers(response.data.data)
+        }
+        fetchData()
+    }, [])
+    console.log(suppliers);
     return (
         <>
             <div className="mt-10 md:mt-18 px-5 md:px-8 lg:px-18">
@@ -22,15 +34,17 @@ const Registered = () => {
                 </div>
 
                 <div className="mt-8 md:mt-10 grid grid-cols-1 sm:grid-cols-2">
-                    <Link href="/registered/07x4....6351" className="bg-[#BEE3F8]/25 hover:bg-[#0D6EFD] w-full px-3 sm:px-4 md:px-6 py-4 md:py-6 rounded-md flex flex-col gap-5 transition-all duration-300">
+                    {suppliers.map((s) => (
+                    <Link key={s.ethWalletAddress} href="/registered/07x4....6351" className="bg-[#BEE3F8]/25 hover:bg-[#0D6EFD] w-full px-3 sm:px-4 md:px-6 py-4 md:py-6 rounded-md flex flex-col gap-5 transition-all duration-300">
                         <div>
-                            <h2 className="text-base sm:text-xl md:text-2xl font-semibold wrap-break-word">PT KOPI INDONESIA</h2>
-                            <p className="text-xs md:text-sm">Toraja, Sulawesi Selatan</p>
+                            <h2 className="text-base sm:text-xl md:text-2xl font-semibold wrap-break-word">{s.supplierName}</h2>
+                            <p className="text-xs md:text-sm">{s.origin}</p>
                         </div>
                         <div>
-                            <p className="text-xs md:text-sm">07x4....6351</p>
+                            <p className="text-xs md:text-sm">{s.ethWalletAddress}</p>
                         </div>
                     </Link>
+                    ))}
                 </div>
             </div>
         </>
