5a5be570b538e745ef1a664f9f4ea28a4215bff2
Fauzan Husain <achmadfauzan1340@gmail.com>
2025-12-31 22:00:02 +0800

    api(services) calling API

---
+++ services/supplier.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import callApi from "@/config/api";
+
+const ROOT_API = "http://localhost:8000";
+const API_VERSION = "api/v1";
+
+export async function notRegisteredSuppliers() {
+    const url = `${ROOT_API}/${API_VERSION}/supplier/not-registered`;
+    return await callApi({
+        url,
+        method: "GET",
+    });
+}
+
+export async function registeredSuppliers() {
+    const url = `${ROOT_API}/${API_VERSION}/supplier/registered`;
+    return await callApi({
+        url,
+        method: "GET",
+    });
+}
+
+export async function detailSupplier(ethWalletAddress) {
+    const url = `${ROOT_API}/${API_VERSION}/supplier/detail/${ethWalletAddress}`;
+    return await callApi({
+        url,
+        method: "GET",
+    });
+}
